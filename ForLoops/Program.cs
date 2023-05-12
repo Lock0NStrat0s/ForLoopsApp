3e4c88b3d875f019d54830d38b5f3cf8787dd89b
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-12 00:25:53 -0600

    code complete.

--- ForLoops/Program.cs
+++ ForLoops/Program.cs
@@ -1,1 +1,29 @@
 ﻿
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"The value of i is {i}");
+//}
+
+//string data = "Tim,Bob,Sue,Jane";
+//List<string> firstNames = data.Split(',').ToList();
+
+//for (int i = 0; i < firstNames.Count; i++)
+//{
+//    Console.WriteLine($"{firstNames[i]} is in attendance.");
+//}
+
+List<decimal> charges = new();
+
+charges.Add(23.78M);
+charges.Add(15.89M);
+charges.Add(125M);
+
+decimal total = 0;
+
+for (int i = 0; i < charges.Count; i++)
+{
+    total += charges[i];
+}
+
+Console.WriteLine($"Our total charge is {total}.");
